5819e1cfa243f533cca5561af32b2b462fd11476
Anas <anasginolt@gmail.com>
2024-10-13 02:05:50 +0530

    JWT  token handling pending

--- routes/auth.js
+++ routes/auth.js
@@ -34,12 +34,12 @@ router.post("/login",async (req,res)=>{
 
         OriginalPassword !==req.body.password && res.status(401).json("Wrong Credentials"); 
 
-        const accesToken=jwt.sign({
+        const accessToken=jwt.sign({
             id:user._id,
             isAdmin:user.isAdmin,
-        },process.env.JWT_SEC)
+        },process.env.JWT_SEC,{expiresIn:"3d"})
         const {password, ...others}=user._doc;   
-        res.status(200).json(others);
+        res.status(200).json({...others,accessToken});
     }catch (err){
         res.sendStatus(500).json(err);
     }
---
+++ routes/verifyToken.js
@@ -0,0 +1,18 @@
+const jwt = require("jsonwebtoken");
+
+const verifyToken= (req,res,next)=>{
+    const authHeader=req.headers.token;
+    if(authHeader){
+        jwt.verify(token,process.env.JWT_SEC,(err,user)=>{
+            if(err)  res.status(401).json("Token is not valid");
+            req.user=user;
+            next();
+        })
+    }else{
+        return res.status(401).json("You are not authenticated")
+    }
+}
+
+
+
+module.exports()
