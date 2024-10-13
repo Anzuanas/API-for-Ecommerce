16852f1f0cb8ddbbd381519a7c5d3133e05dd784
Anas <anasginolt@gmail.com>
2024-10-13 16:35:24 +0530

    jwt token completed

--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -3,6 +3,7 @@ const jwt = require("jsonwebtoken");
 const verifyToken= (req,res,next)=>{
     const authHeader=req.headers.token;
     if(authHeader){
+        const token=authHeader.split(" ")[1]
         jwt.verify(token,process.env.JWT_SEC,(err,user)=>{
             if(err)  res.status(401).json("Token is not valid");
             req.user=user;
@@ -13,6 +14,26 @@ const verifyToken= (req,res,next)=>{
     }
 }
 
+const verifyTokenAndAuthorization=(req,res,next)=>{
+    verifyToken(req,res,()=>{
+        if(req.user.id===req.params.id||req.user.isAdmin){
+            next();
+        }else{
+            res.status(403).json("You are not allowed to do that")
+        }
+    })
+}
+
+const verifyTokenAndAdmin=(req,res,next)=>{
+    verifyToken(req,res,()=>{
+        if(req.user.isAdmin){
+            next();
+        }else{
+            res.status(403).json("You are not allowed to do that")
+        }
+    })
+}
 
 
-module.exports()
+
+module.exports = { verifyToken, verifyTokenAndAuthorization,verifyTokenAndAdmin };
