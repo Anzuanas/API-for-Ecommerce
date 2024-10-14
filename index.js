cea33273552e21ec37b7d3b50fe863b92e2b910a
Anas <anasginolt@gmail.com>
2024-10-14 08:06:40 +0530

    All pages for rest api is completed except payment

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const mongoose=require("mongoose")
 const dotenv=require("dotenv")
 const userRoute=require("./routes/user")
 const authRoute=require("./routes/auth")
+const productRoute=require("./routes/product")
+const cartRoute=require("./routes/cart")
+const orderRoute=require("./routes/order")
 
 dotenv.config();
 
@@ -18,6 +21,9 @@ mongoose.connect(
 app.use(express.json());
 app.use("/api/auth",authRoute);
 app.use("/api/users",userRoute);
+app.use("/api/products",productRoute);
+app.use("/api/carts",cartRoute);
+app.use("/api/orders",orderRoute);
 
 app.listen(process.env.port||5000, ()=>{
     console.log("Backend server is running")
--- routes/product.js
+++ routes/product.js
@@ -1,17 +1,30 @@
 const router=require("express").Router();
-const User = require("../models/User");
+const Product = require("../models/Product");
 const {verifyToken,verifyTokenAndAuthorization, verifyTokenAndAdmin}=require("./verifyToken")
-router.put("/:id",verifyTokenAndAuthorization,async(req,res)=>{
-    if(req.body.password){
-        req.body.password=CryptoJS.AES.encrypt(req.body.password,process.env.PASS_SEC).toString();
-    }
+
+//CREATE
+router.post("/",verifyTokenAndAdmin,async(req,res)=>{
+    const newProduct=new Product(req.body)
+
     try{
-        const updatedUser= await User.findByIdAndUpdate(req.params.id,{
+        const savedProduct=await newProduct.save();
+        res.status(200).json(savedProduct);
+
+    }catch(err){
+        res.status(500).json(err)
+    }
+})
+
+//UPDATE 
+router.put("/:id",verifyTokenAndAdmin,async(req,res)=>{
+    
+    try{
+        const updatedProduct= await Product.findByIdAndUpdate(req.params.id,{
             $set:req.body,
         },{
             new:true
         })
-        res.status(500).json(updatedUser)
+        res.status(500).json(updatedProduct)
     }catch(err){
         res.status(500).json(err)
     }
@@ -19,64 +32,50 @@ router.put("/:id",verifyTokenAndAuthorization,async(req,res)=>{
 
 //DELETE
 
-router.delete("/:id",verifyTokenAndAuthorization,async (req,res)=>{
+router.delete("/:id",verifyTokenAndAdmin,async (req,res)=>{
     try{
-        await User.findByIdAndDelete(req.params.id);
-        res.status(200).json("User has been deleted")
+        await Product.findByIdAndDelete(req.params.id);
+        res.status(200).json("Product has been deleted")
     }catch(err){
         res.status(500).json(err);
     }
 })
 
-//GET USER
+//GET PRODUCT
 
-router.get("/find/:id",verifyTokenAndAdmin,async (req,res)=>{
+router.get("/find/:id",async (req,res)=>{
     try{
-       const user= await User.findById(req.params.id);
-       const {password, ...others}=user._doc;   
-       res.status(200).json(others);
+       const product= await Product.findById(req.params.id);   
+       res.status(200).json(product);
     }catch(err){
         res.status(500).json(err);
     }
 })
 
-//GET ALL USER
+//GET ALL PRODUCT
 
 router.get("/",verifyTokenAndAdmin,async (req,res)=>{
-    const query=req.query.new; 
+    const qNew=req.query.new;
+    const qCategory=req.query.category; 
     try{
-       const users=query ?await User.find().sort({_id:-1}).limit(1): await User.find();  
-       res.status(200).json(users);
+       let products;
+       if(qNew){
+        products=await Product.find().sort({createAt:-1}).limit(1)
+       }else if(qCategory){
+        products=await Product.find({
+            categories:{
+                $in:[qCategory],
+            }
+        });
+       }else{
+        products=await Product.find();
+       }
+       res.status(200).json(products);
     }catch(err){
         res.status(500).json(err);
     }
 })
 
-//GET USER STATS
 
-router.get("/stats",verifyTokenAndAdmin, async (req,res)=>{
-    const date=new Date();
-    const lastYear=new Date(date.setFullYear(date.getFullYear()-1));
-
-    try{
-        const data= await User.aggregate([
-            {$match:{createdAt:{$gte:lastYear}}},
-        {
-            $project:{
-                month:{$month:"$createdAt"}
-            }
-        },
-        {
-            $group:{
-                _id:"$month",
-                total:{$sum:1}
-            }
-        }
-        ])
-        res.status(200).json(data)
-    }catch(err){
-        res.status(500).json(err)
-    }
-})
 
 module.exports=router;
